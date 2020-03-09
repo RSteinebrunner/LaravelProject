32c904526f94f0b933f2f628522d330f3f5e1b0d
Roland Steinebrunner <steinebrunner.r@gmail.com>
2020-03-08 20:40:08 -0700

    Revert "fix"
    This reverts commit 77256227186e52d53e7220487f5043b42b06316f.

---
+++ resources/views/Controllers/EducationController.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Http\Controllers;
+
+/*
+ Project name/Version: LaravelCLC Version: 3
+ Module name: Education Moduke
+ Authors: Roland Steinebrunner, Anthony Clayton
+ Date: 2/23/2020
+ Synopsis: handles all features reagarding changing a user's education
+ Version#: 1
+ References: N/A
+  */
+use App\Models\UserModel;
+use App\Models\EducationModel;
+use App\Services\Business\EducationService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+//controller hold basic methods to either route to other views or request securityservice for further user specific actions
+class EducationController extends Controller{
+    
+    public function deleteEducation(Request $request){
+        //get the id
+        $id = $request->input('id');
+        //create new service
+        $users = new EducationService();
+        $result = $users->deleteEducation($id);
+        if($result){
+            return redirect()->route('portfolio');
+        }
+        else{
+            return view('managerError');
+        }
+    }
+    public function addEducation(Request $request){
+        
+        $this->validateForm($request);
+        //pull form data to make user
+        $id = $request->input('id');
+        $years = $request->input('years');
+        $degree = $request->input('degree');
+        $school = $request->input('school');
+        
+        //create new object
+        $newEdu = new EducationModel(NULL, $years, $degree, $school);
+        //pass the person object to the security service
+        $service = new EducationService();
+        $result = $service->addEducation($newEdu, $id);
+        $data = $service->findAllEducation($id);
+        if($result == "true"){
+            return redirect()->route('portfolio');
+        }
+        else {
+            return view('profileDisplayError')->with("data","educationInsert");
+        }
+    }
+    private function validateForm(Request $request)
+    {
+        // Setup Data Validation Rules for Login Form
+        $rules = ['years' => 'Required | Between:4,5',
+            'degree' => 'Required | Between:1,30',
+            'school' => 'Required | Between:1,30'
+           ];
+        
+        // Run Data Validation Rules
+        $this->validate($request, $rules);
+    }
+    
+    
+    
+  
+}
