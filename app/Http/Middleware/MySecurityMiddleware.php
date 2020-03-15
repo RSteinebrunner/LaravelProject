d02dd4d8d0f519f2023cb23a713312a94b6c6145
Jack Sidrak <JSidrak@my.gcu.edu>
2020-03-14 22:10:23 -0700

    Added Middleware Security & showJobSearchPosting

---
+++ app/Http/Middleware/MySecurityMiddleware.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+/*
+ Project name/Version: LaravelCLC Version: 5
+ Module name: Security Module
+ Authors: Jack Setrak
+ Date: 03/14/2020
+ Synopsis: Security middleware that will redirect anyone trying to gain access to pages they don't have permission to
+ Version#: 1
+ References: N/A
+ */
+
+use Closure;
+
+class MySecurityMiddleware
+{
+    /**
+     * Handle an incoming page request and make sure the necessary permissions are met.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next){
+        $path = $request->path();
+        
+        //exception pages that don't require a user to be logged in 
+        $secureCheck = true;
+        if($request->is('/') ||
+            $request->is('login') ||            
+            $request->is('doLogin') || 
+            $request->is('register') || 
+            $request->is('doRegister'))
+        {
+            $secureCheck = false;    
+        }
+        
+        $enable = true;
+        //if username is set in session then a user is logged in 
+        if($request ->session()->get('username') != null){
+            $enable = false;
+        }
+        
+        //if the user isn't logged and the page isn't one of the exception pages above, then redirect the user back to the login page
+        if($enable && $secureCheck){
+            return redirect('/login');
+        }
+        
+        //proceed as normal to next middleware in chair
+        return $next($request);
+    }
+}
---
+++ resources/views/showJobSearchPosting.blade.php
@@ -0,0 +1,85 @@
+<!--  
+Project name/Version: LaravelCLC Version: 5
+Module name: Portfolio Module
+Authors: Roland Steinebrunner, Anthony Clayton
+Date: 3/9/2020
+Synopsis: View page that shows the login page when a user attempts to sign into their account
+Version#: 3
+References: N/A
+-->
+
+@extends('layouts.appmaster')
+@section('head','Job Search Posting')
+
+@section('title')
+    	<h2>Job Search Postings</h2>
+@endsection
+
+@section('content')
+<!-- Job Posting -->
+    <div class = "container">   
+    <div class= "row justify-content-center">
+    	<div class = "col">
+    	<table class="table table-dark">
+    	<tr><th scope="col">Company</th>
+    	<th scope="col">Position</th>
+    	<th scope="col">Description</th>
+<!--    <th scope="col">Requirements</th>
+    	<th scope="col">Pay</th>
+    	<th scope="col">Posting Date</th> -->
+    	<th scope="col">Interested?</th>
+    	@if(Session::get('Role') == "admin")
+    	<th scope="col">*Actions*</th>    	
+    	@endif
+    	</tr>
+        		@foreach($result as $job)
+        		<tr>
+        			<td>{{$job->getCompany()}}</td>
+        			<td>{{$job->getPosition()}}</td>
+        			<td>{{$job->getDescription()}}</td> 
+<!--        		<td>{{$job->getRequirements()}}</td>
+         			<td>{{$job->getPay()}}</td>
+        			<td>{{$job->getPostingDate()}}</td>  -->
+        			<td>
+    					<button class="btn btn-success" type="submit">Apply</button>
+        			</td>
+        			@if(Session::get('Role') == "admin")
+        			<td>
+        			<form action="adminJobPosting">
+        				<input type="hidden" name="_token" value="{{csrf_token()}}" />
+    					<input type="hidden" name="id" value="{{$job->getJobId()}}"> 
+        			    <button class="btn btn-warning" type="submit">Edit</button>       			
+        			</form>
+        			<button type="button" class="btn btn-danger" data-toggle="modal"
+							data-target="#delete{{$job->getJobId()}}">Delete</button>
+        			</td>	
+        			@endif	
+        		</tr>
+        		<!-- The Modal -->
+				<div class="modal fade" id="delete{{$job->getJobId()}}">
+					<div class="modal-dialog modal-dialog-centered modal-sm">
+						<div class="modal-content">
+
+							<!-- Modal Header -->
+							<div class="modal-header">
+								<h4 class="modal-title text-center">Delete Job Posting?</h4>
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+							</div>
+
+							<!-- Modal body -->
+							<div class="modal-body" >
+								<form method="post" action="deleteJobPosting">
+    								<input type="hidden" name="_token" value="{{csrf_token()}}" />
+    								<input type="hidden" name="id" value="{{$job->getJobId()}}"> 
+    								<button class="btn btn-danger" type="submit">Delete</button>
+    							</form>	
+							</div>
+						</div>
+					</div>
+				</div>
+        		@endforeach
+        </table>
+        </div>
+     </div>
+     
+@endsection
